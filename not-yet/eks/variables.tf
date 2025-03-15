e09dc1af6ceb79368917455447c1a9675855701a
s33ding <robertomdiniz@protonmail.com>
2025-03-15 19:10:29 -0300

    saving

--- not-yet/eks/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cluster_name" {}
-variable "cluster_version" {}
-variable "subnet_ids" { type = list(string) }
-variable "vpc_id" {}
-variable "node_group_desired_size" {}
-variable "node_group_min_size" {}
-variable "node_group_max_size" {}
-variable "node_group_instance_types" { type = list(string) }
-
